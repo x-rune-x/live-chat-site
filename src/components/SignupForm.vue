b91f5fee1434c6192345ddbd7ee640697d46e44e
James Bedford <jamesbedford@protonmail.com>
2022-01-13 23:48:12 +1100

    Connected log in and sign up functions to Firebase using Auth.

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -3,24 +3,30 @@
     <input type="text" required placeholder="display name" v-model="displayName">
     <input type="email" required placeholder="email" v-model="email">
     <input type="new-password" required placeholder="password" v-model="password">
+    <div class="error">{{ error }}</div>
     <button>Sign up</button>
   </form>
 </template>
 
 <script>
 import { ref } from 'vue'
+import useSignup from '../composables/useSignup'
+
 export default {
   setup() {
+    const { error, signup} = useSignup()
+
     //refs
     const displayName = ref('')
     const email = ref('')
     const password = ref('')
 
-    const handleSubmit = () => {
-      console.log(displayName.value, email.value, password.value)
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, displayName.value) 
+      console.log('User signed up.')
     }
 
-    return { displayName, email, password, handleSubmit }
+    return { displayName, email, password, handleSubmit, error }
   }
 }
 </script>
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,6 @@
 import firebase from "firebase/app"
 import 'firebase/firestore'
+import 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyCMphCFPZGfNrcxi9nRoZdp_HqsP-oCdkw",
@@ -13,6 +14,7 @@ const firebaseConfig = {
 // init firebase
 firebase.initializeApp(firebaseConfig)
 const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
 const timestamp = firebase.firestore.FieldValue.serverTimestamp
 
-export { projectFirestore, timestamp }
+export { projectFirestore, timestamp, projectAuth }
